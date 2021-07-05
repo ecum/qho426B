283802fdfa06b9e4d28e7e6ee770edf1ba88a97f
622021 <>
2021-07-05 13:23:38 +0000

    Update code to display a longer multi-line messages

--- basic/output/multilines_messages.py
+++ basic/output/multilines_messages.py
@@ -1,3 +1,9 @@
 #Display message to the standard output
 print("System failure imminent !")
 print("System rebbot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine ")
+print()
+print(" system online.")
