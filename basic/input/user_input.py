3dd0a5c3476fe8f752e2f16a13034ff6021cd2b7
622021 <>
2021-07-10 14:04:19 +0000

    added code to read user input

---
+++ basic/input/user_input.py
@@ -0,0 +1,12 @@
+name = "marcelo"
+aluna = "erica"
+#print("O professor ",name,"está dando aula para ", aluna,end="#")
+#print("O professor {}  esta dando aula para {}" .format(name, aluna))
+
+# Ask user to enter their name
+print(" What is your name human?")
+name = input()
+print("It is nice to meet you too {}" .format(name) ,"!")
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+print("Program Started !")
+a2 = abs(int(input (" Please enter an ASCII code:")))
+if a2  in  range(32,127):
+  print("The character represented by the ASCII code {} is {}".format(a2, chr(a2)))
+else:
+   print("This is an error!Invalid number code")
+print("Program has ended!")
